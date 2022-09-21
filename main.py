71b487e7761b8773a6c7fb4725ca9db199108e51
Alex Mann <amann@student.hive.fi>
2022-09-21 18:17:41 +0300

    now added a process for reading and deleting messages, one by one. No handling for reading multiple messages at a time in the program itself yet, the options selected by the user will be ignored.

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ import boto3
 import base64
 import json
 import sys
+from botocore.exceptions import ClientError
 from try_parse_uint import *
 
 # messages read and visibility timeout must be configurable, so these can be read from cmd line
@@ -41,20 +42,45 @@ client = session.client(
     aws_session_token='SESSION_TOKEN'
 )
 
-"""reading from the queue"""
+def print_message_body(message):
+     #decoding to json
+     code = message['Messages'][0]['Body']
+     decoded_bytes = base64.standard_b64decode(code)
+     decoded_str = decoded_bytes.decode("ascii")
+     json_str=json.loads(decoded_str)
+     print(json.dumps(json_str, indent=4))
 
-response = client.receive_message(
-        QueueUrl='http://localstack:4566/000000000000/submissions',
+def delete_message(message):
+    try:
+        response = client.delete_message(
+            QueueUrl='http://localstack:4566/000000000000/submissions',
+            ReceiptHandle=message['Messages'][0]['ReceiptHandle']
+        )
+    except ClientError:
+        print("couldn't delete messages from the queue")
+    else:
+        return response
 
-    )
+#reading from the queue
+def receive_next_message():
+    try:
+        message = client.receive_message(
+                QueueUrl='http://localstack:4566/000000000000/submissions',
+                #MaxNumberOfMessages=num_of_messages,
+                #VisibilityTimeout=visibility_timeout
+        )
+        print_message_body(message)
+    except ClientError as error:
+        print("Couldn't receive messages from the queue")
+        raise error
+    else:
+        return message
 
-"""decoding to json"""
-
-code = response['Messages'][0]['Body']
-decoded_bytes = base64.standard_b64decode(code)
-decoded_str = decoded_bytes.decode("ascii")
-json_str=json.loads(decoded_str)
-
-"""debug print for testing"""
-print(json.dumps(json_str, indent=4))
-
+more_messages = True
+while more_messages:
+    received_message = receive_next_message()
+    if received_message:
+        delete_message(received_message)
+    else:
+        more_messages = False
+print("done")
